cb4ec17a5f0b2fdcd02cfe2f7fcfc81146215870
tomasgoico7 <tomasgoico7@gmail.com>
2024-03-14 12:42:05 -0300

    app carrito compras

---
+++ src/CarritoApp.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { ComprasPage } from "./pages/ComprasPage";
+import { CarritoPage } from "./pages/CarritoPage";
+import { ProductosProvider } from "./context/ProductosProvider";
+import { CarritoProvider } from "./context/CarritoProvider";
+
+export const CarritoApp = () => {
+  return (
+    <ProductosProvider>
+      <CarritoProvider>
+        <NavBar></NavBar>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ComprasPage></ComprasPage>}></Route>
+            <Route
+              path="/carrito"
+              element={<CarritoPage></CarritoPage>}
+            ></Route>
+            <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
+          </Routes>
+        </div>
+      </CarritoProvider>
+    </ProductosProvider>
+  );
+};
